a77b46b4edc92d9902e520e5be6ca485d4bd5642
athlcn <athlcn@163.com>
2018-05-08 09:49:31 +0800

    :Added Java基本数据类型

---
+++ src/xxl/ch1/PrimitiveTypeTest.java
@@ -0,0 +1,27 @@
+package xxl.ch1;
+
+public class PrimitiveTypeTest {
+	public static void main(String[] args) {
+		
+		System.out.println("Java中的8种数据类型:byte、short、int、long、float、double、char、boolean");
+		// byte
+		System.out.println("byte 内存空间 " + Byte.SIZE + " 位 , 取值范围 " + Byte.MIN_VALUE + " 到 " + Byte.MAX_VALUE);
+		// short
+		System.out.println("short 内存空间 " + Short.SIZE + " 位 , 取值范围 " + Short.MIN_VALUE + " 到 " + Short.MAX_VALUE);
+		// int
+		System.out.println("int 内存空间 " + Integer.SIZE + " 位 , 取值范围 " + Integer.MIN_VALUE + " 到 " + Integer.MAX_VALUE);
+		// long
+		System.out.println("long 内存空间 " + Long.SIZE + " 位 , 取值范围 " + Long.MIN_VALUE + " 到 " + Long.MAX_VALUE);
+		// float
+		System.out.println("float 内存空间 " + Float.SIZE + " 位 , 取值范围 " + Float.MIN_VALUE + " 到 " + Float.MAX_VALUE);
+		// double
+		System.out.println("double 内存空间 " + Double.SIZE + " 位 , 取值范围 " + Double.MIN_VALUE + " 到 " + Double.MAX_VALUE);
+		// char
+		System.out.println("char 内存空间 " + Character.SIZE + " 位 , 取值范围 " + (int)Character.MIN_VALUE + " 到 " + (int)Character.MAX_VALUE);
+					
+	
+	
+	
+	}
+
+}
